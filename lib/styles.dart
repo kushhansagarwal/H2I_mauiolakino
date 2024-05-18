2f67fce0e9a0ee97da5f9201b1be7726f92b2f33
Kush Agarwal <kushagarwal@ucla.edu>
2024-05-17 22:51:11 -0700

    first commit

---
+++ lib/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle heading = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle subheading = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle paragraph = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.normal,
+  );
+}
